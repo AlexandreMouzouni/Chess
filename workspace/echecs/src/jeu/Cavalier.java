c0e5119b0e7b5fe37ef410cfde9b492ea2b8f639
xy2 <xy2@cock.li>
2019-05-08 16:41:07 +0000

    Update Cavalier.java

--- workspace/echecs/src/jeu/Cavalier.java
+++ workspace/echecs/src/jeu/Cavalier.java
@@ -5,7 +5,12 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 @objid ("7d04237b-e42b-4a58-9729-9ab91afeee7c")
 public class Cavalier extends Piece {
     @objid ("667cac35-5223-487f-a37d-c2a99a0a0cb4")
-    public void coupValide(Position pos) {
+    /*
+     * horizontal: déplacement horizontal
+     * vertical: déplacement vertical 
+     */
+    public void coupValide(int horizontal, int vertical) {
+        return ( (Math.abs(horizontal) == 2 && Math.abs(vertical) == 1) 
+			|| (Math.abs(horizontal) == 1 && Math.abs(vertical) == 2) );
     }
-
 }
